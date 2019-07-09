84d309914d9f07efbae79f69e46868cd82ac63bb
vatsal <vatsalkulshreshtha@gmail.com>
2019-07-09 12:20:00 +0530

    Initial Commit

---
+++ CalencyTests/CalencyTests.swift
@@ -0,0 +1,34 @@
+//
+//  CalencyTests.swift
+//  CalencyTests
+//
+//  Created by Vatsal Kulshreshtha on 09/07/19.
+//  Copyright © 2019 Vatsal Kulshreshtha. All rights reserved.
+//
+
+import XCTest
+@testable import Calency
+
+class CalencyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
